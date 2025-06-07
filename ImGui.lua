68d13ec64f2cad9e93e15fcbf5ddf05472e1e031
s1cht <sachnikitus@gmail.com>
2025-06-07 12:40:33 +0300

    ImGui.lua updated

--- ImGui.lua
+++ ImGui.lua
@@ -5,8 +5,8 @@ project "ImGui"
 	language "C++"
     staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
